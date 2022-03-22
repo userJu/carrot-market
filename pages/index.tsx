b9906a6799b0f6959f0b27bac007e71ccc3f6e25
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-03-22 12:01:11 +0900

    Tailwind css 설치, npx tailwind init을 이용해 파일 설치, tailwind 파일 위치 지정, globalStyle에 Tailwind 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+// Next.js에서는 type을 이런 식으로 가져온다 import type
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
 };
 
 export default Home;
